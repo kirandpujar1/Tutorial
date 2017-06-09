0178d651c8c592d2bdb00fd1909ebf7ea0314307
kiran <kiran@neha-ishan>
2017-06-09 19:49:52 +0530

    hh

--- src/test/java/maven/demo/Apptest.java
+++ src/test/java/maven/demo/Apptest.java
@@ -5,7 +5,7 @@ import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class Apptest {
-	//hi hello neha ishan
+	//hi hello neha ishan fff
 @Test
 public void test() {
 	//Assert.fail();
